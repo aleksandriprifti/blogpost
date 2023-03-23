1bcce3e2570382f3021e399cf3dc253c62696015
aleksandriprifti <prifti.aleksandri@gmail.com>
2023-03-23 10:34:34 +0100

    create project

---
+++ src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Modal.module.css';
+
+export const Modal = ({ children, onClose }) => {
+    return (
+        <>
+            <div className={classes.backdrop} onClick={onClose} />
+            <dialog open className={classes.modal}>{children}</dialog>
+        </>
+    )
+}
